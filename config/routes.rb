979c7ccc273be764026ee02e6211828a6cdfc109
jevaisfaire <34928082+jevaisfaire@users.noreply.github.com>
2018-01-02 14:54:19 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,5 @@ Rails.application.routes.draw do
   resources :messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+resources:messages
